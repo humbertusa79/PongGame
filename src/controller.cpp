c7f4c3a873633124ec8b31612ac62ae15c7be314
JoseHumbertoPartidaGarduno <humbertusa79@gmail.com>
2020-07-25 17:05:37 -0700

    Chore: Clean the snake code to use it as base for the pong game

---
+++ src/controller.cpp
@@ -0,0 +1,15 @@
+#include "controller.h"
+#include <iostream>
+#include "SDL.h"
+
+void Controller::HandleInput(bool &running) const {
+  SDL_Event e;
+  while (SDL_PollEvent(&e)) {
+    if (e.type == SDL_QUIT) {
+      running = false;
+    } else if (e.type == SDL_KEYDOWN) {
+      switch (e.key.keysym.sym) {
+      }
+    }
+  }
+}
