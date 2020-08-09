853d50c8c968bba2074ab4b67b425b98a152e120
JoseHumbertoPartidaGarduno <humbertusa79@gmail.com>
2020-08-08 20:18:28 -0700

    Task: increase the window width

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main() {
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
-  constexpr std::size_t kScreenWidth{640};
+  constexpr std::size_t kScreenWidth{960};
   constexpr std::size_t kScreenHeight{640};
 
   Renderer renderer(kScreenWidth, kScreenHeight);
